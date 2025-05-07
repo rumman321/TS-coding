1eb46e72222e884828aedd24bb6e78f34e617dac
rumman321 <cset-210652@dti.ac>
2025-05-07 07:34:59 +0600

    create function type guard using in

--- module3/3.3.ts
+++ module3/3.3.ts
@@ -1,5 +1,5 @@
 {
-    //type guard
+    //type guard using typeof
     const add = (param1:string|number, param2:string|number) :string|number =>{
         if(typeof param1 === 'string' && typeof param2 === 'string'){
             console.log(param1+param2);
@@ -17,4 +17,33 @@
     }
 
     add('3','5')
+
+    //type guard using in
+
+    type NormalUser ={
+        name:string
+    }
+    type AdminUser = {
+        name:string;
+        role:'admin'
+    }
+    const getUser = (user: NormalUser | AdminUser)=>{
+       
+        if('role' in user ){
+            console.log(`${user.name} is Admin`);
+        }else{
+            console.log(`${user.name} in normal user`);
+        }
+    }
+
+    const normal : NormalUser = {
+        name:'rumman'
+    }
+    const admin : AdminUser = {
+        name:'Mahfuj',
+        role:'admin'
+    }
+
+    getUser(admin)
+
 }
